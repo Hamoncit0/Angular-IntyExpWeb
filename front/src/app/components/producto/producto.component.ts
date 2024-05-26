03cd3a519d6421abafa02f5d82045b3500dcd66c
Hamoncit0 <151789307+Hamoncit0@users.noreply.github.com>
2024-05-26 00:36:51 -0600

    no carrito sin user

--- front/src/app/components/producto/producto.component.ts
+++ front/src/app/components/producto/producto.component.ts
@@ -4,6 +4,7 @@ import { ProductService } from '../../service/product.service';
 import { Product } from '../../interfaces/product';
 import { Router } from '@angular/router';
 import { NotificationService } from '../../service/notification.service';
+import { AutenticacionService } from '../../service/autenticacion.service';
 @Component({
   selector: 'app-producto',
   standalone: true,
@@ -20,7 +21,7 @@ export class ProductoComponent implements OnChanges{
   @Input() precio: number = 0;
   @Input() idCat: number = 0;
   producto:Product ={IdProducto:this.id, Nombre:this.nombre, Marca:this.marca,Detalles:this.descripcion, Imagen:this.imagenUrl, Precio:this.precio, IdCategoria:this.idCat, Cantidad:0}
-  constructor(public productService:ProductService, private router:Router, private notificationService:NotificationService){
+  constructor(public productService:ProductService, private router:Router, private notificationService:NotificationService, public authService:AutenticacionService){
 
   }
   ngOnChanges(changes: SimpleChanges): void {
@@ -40,14 +41,17 @@ export class ProductoComponent implements OnChanges{
 
   
   addToCart() {
-    // Aquí va la lógica para agregar al carrito UwU
-    console.log('Producto añadido al carrito:', this.producto);
-    this.productService.addToCart(this.producto).subscribe(response => {
-      console.log('Producto añadido al carrito:', this.producto);
-    }, error => {
-      console.error('Error al agregar al carrito:', error);
-    });
-    this.notificationService.showNotification('Producto agregado con éxito');
+    if(this.authService.currentUser){
+        // Aquí va la lógica para agregar al carrito UwU
+        console.log('Producto añadido al carrito:', this.producto);
+        this.productService.addToCart(this.producto).subscribe(response => {
+          console.log('Producto añadido al carrito:', this.producto);
+        }, error => {
+          console.error('Error al agregar al carrito:', error);
+        });
+        this.notificationService.showNotification('Producto agregado con éxito');
+    }
+   
   }
   verMas(){
     this.router.navigate(['/producto', this.producto.IdProducto]);
